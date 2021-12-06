132f84db93bf5d08aacaa92927baeedeb35ce35c
random-raccoon <rnd.raccoon@gmail.com>
2021-12-06 13:00:11 -0500

    Day 6 complete.
    Definitely went with the right approach in part 1 :)

---
+++ day06/part1.py
@@ -0,0 +1,21 @@
+with open('day06/input.txt', 'r') as f:
+    population = [int(v) for v in f.read().strip().split(',')]
+    buckets = [0] * 9
+
+    # Initialize
+    for p in population:
+        buckets[p] += 1
+
+    for i in range(0, 80):
+        newBuckets = [0] * 9
+        newBuckets[8] = buckets[0]
+        newBuckets[6] = buckets[0]
+        for age in range(1, 9):
+            newBuckets[age-1] += buckets[age]
+        buckets = newBuckets
+
+    count = 0
+    for c in buckets:
+        count += c
+
+    print(count)
