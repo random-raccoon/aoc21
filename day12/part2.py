7cbc67bf54f5872f9b4c185977483da7fd8acec7
random-raccoon <rnd.raccoon@gmail.com>
2021-12-15 13:54:36 -0500

    Switch from recursive to iterative solution.
    And now it works like a charm.

--- day12/part2.py
+++ day12/part2.py
@@ -1,3 +1,21 @@
+class Path:
+    def __init__(self):
+        self.caves = []
+        self.has_second_visit = False
+    
+    def with_new_cave(self, cave: str):
+        p = Path()
+        p.has_second_visit = self.has_second_visit
+        p.caves = self.caves.copy()
+        p.caves.append(cave)
+        return p
+
+    def current_cave(self):
+        return self.caves[len(self.caves)-1]
+
+    def contains(self, cave):
+        return cave in self.caves
+
 class CaveNetwork:
     def __init__(self):
         self.caves = {}
@@ -13,27 +31,36 @@ class CaveNetwork:
             self.caves[cave] = set()
 
     def count_paths(self) -> int:
-        return self.__explore([], 'start', False)
-
-    def __explore(self, path: list[str], next_cave: str, has_second_visit: bool) -> int:
         count = 0
-        for cave in self.caves[next_cave]:
-            if cave == 'end':
-                p = path.copy()
-                p.append('end')
-                print(','.join(p))
-                count += 1
-            elif cave == 'start':
-                # no leaving the cave.
-                continue
-            elif cave.islower() and cave in path:
-                if has_second_visit:
+
+        start_path = Path()
+        start_path.caves.append('start')
+        paths = [start_path]
+
+        while len(paths) > 0:
+            path = paths.pop()
+            cave = path.current_cave()
+            for next_cave in self.caves[cave]:
+                if next_cave == 'end':
+                    # Made it out!  Record it.
+                    count += 1
+                elif next_cave == 'start':
+                    # No leaving the cave this way.
                     continue
+                elif next_cave.islower() and path.contains(next_cave):
+                    if path.has_second_visit:
+                        # No revisiting caves any more in this path.
+                        continue
+                    else:
+                        next_path = path.with_new_cave(next_cave)
+                        next_path.has_second_visit = True
+                        paths.append(next_path)
                 else:
-                    count += self.__explore(path.copy(), cave, True)
-            else:
-                count += self.__explore(path.copy(), cave, has_second_visit)
+                    next_path = path.with_new_cave(next_cave)
+                    paths.append(next_path)
+
         return count
+
 
 with open('day12/input.txt', 'r') as f:
     network = CaveNetwork()
